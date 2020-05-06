a9a978794fc2e9097530fc29c457cbdad90e6581
AureGitHub <auredeveloper@gmail.com>
2020-05-07 00:45:13 +0200

    a pro

--- api/partidoxjugador/partidoxjugador.controller.js
+++ api/partidoxjugador/partidoxjugador.controller.js
@@ -1,9 +1,6 @@
 const db = require('../../database');
 const busOwn = require('./bussines');
 const busPartido = require('../partido/bussines');
-const buspaxpixma = require('../partidoxpistaxmarcador/bussines');
-const buspaxpi = require('../partidoxpistaxjugador/bussines');
-const buspaxpa = require('../partidoxpareja/bussines');
 const busju = require('../jugador/bussines');
 
 const { enumType } = require('../../utils/enum.util');
