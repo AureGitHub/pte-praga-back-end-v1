8ac259391768a4eec44edab5d4e4896e6b9e4722
AureGitHub <auredeveloper@gmail.com>
2020-12-11 15:33:45 +0100

    cambio acceso a BD

--- database/index.js
+++ database/index.js
@@ -4,23 +4,13 @@ const { databaseConfig } = require('../config');
 
 module.exports = knex({
   client: 'pg', // pg is the database library for postgreSQL on knexjs
-  connection: databaseConfig.DATABASE_URL
-  // connection: {
-  //   host: databaseConfig.host, // Your local host IP
-  //   user: databaseConfig.user, // Your postgres user name
-  //   password: databaseConfig.password,
-  //   database: databaseConfig.database,
-  //   port: databaseConfig.port,
-  // },
-  // connection: {
-  //   host: databaseConfig.host, // Your local host IP
-  //   user: databaseConfig.user, // Your postgres user name
-  //   password: databaseConfig.password,
-  //   database: databaseConfig.database,
-  //   port: databaseConfig.port,
-   
-  // },
-  // connection: {
-  //   database: databaseConfig.DATABASE_URL,
-  // },
+  //  connection: databaseConfig.DATABASE_URL
+  connection: {
+    host: databaseConfig.host, // Your local host IP
+    user: databaseConfig.user, // Your postgres user name
+    password: databaseConfig.password,
+    database: databaseConfig.database,
+    port: databaseConfig.port,
+    ssl: { rejectUnauthorized: false }
+  },  
 });
